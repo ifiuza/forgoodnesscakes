6bde1dcc1cfe229ac77278c5733a538743af41e3
Isaac Fiuza <fiuzaisaac@hotmail.com>
2019-10-14 18:40:41 -0600

    adding files and main page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class App extends Component {
+  render(){
+    return <div>
+
+      <h1 style={{color: "red"}}>This will be Our Main Page !!</h1>
+      <h2 style={{color: "blue"}}>Welcome to For Goodness Cakes!!</h2>
+
+
+    </div>
+  }
+}
+
+export default App;
